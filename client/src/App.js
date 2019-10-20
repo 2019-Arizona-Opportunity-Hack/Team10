073d5da2e8edd31f24015bcd365000ba124a645f
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-10-19 19:33:41 -0700

    cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PatientForm from "./Forms/PatientForm.jsx";
 import ManagerDashboard from "./Views/ManagerDashboard";
+import TherapistForm from "./Forms/TherapistForm.jsx"
 
 export default function App() {
   return (
