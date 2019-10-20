317c8999d7e21580102f1da81d26160b8929bb2c
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-10-20 09:23:16 -0700

    fixed merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import PatientForm from './Forms/PatientForm.jsx';
-import ManagerDashboard from './Views/ManagerDashboard';
-import TherapistForm from './Forms/TherapistForm.jsx'
+import React from "react";
+import PatientForm from "./Forms/PatientForm.jsx";
+import ManagerDashboard from "./Views/ManagerDashboard";
+import TherapistForm from "./Forms/TherapistForm.jsx";
 // import HomeScreen from './Views/HomeScreen.jsx'
-import Login from './Views/Login.jsx'
+import Login from "./Views/Login.jsx";
 
-import { Navbar, Nav, Button } from 'react-bootstrap'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <PatientForm /> */}
-      <ManagerDashboard />
-=======
       <Router>
         <Header />
-
         <Switch>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/login" component={Login} />
@@ -27,40 +21,32 @@ export default function App() {
           <Route exact path="/dashboard" component={ManagerDashboard} />
         </Switch>
       </Router>
->>>>>>> 8980507c037b4f5914ef96928ec63216a21956fa
     </div>
   );
 }
 
-function HomeScreen(){
+function HomeScreen() {
   return (
     <div>
-      <Link to='/therapist'>
-        I am a therapist
-      </Link>
-      <Link to='/patient'>
-        I am a patient
-      </Link>
+      <Link to="/therapist">I am a therapist</Link>
+      <Link to="/patient">I am a patient</Link>
     </div>
-
-  )
+  );
 }
 
 function Header() {
   return (
     <div>
-
       <Navbar bg="light" expand="lg">
         <Link to="/">
-          <Navbar.Brand>Neurologic Music Therapy Services of Arizona</Navbar.Brand>
+          <Navbar.Brand>
+            Neurologic Music Therapy Services of Arizona
+          </Navbar.Brand>
         </Link>
         <Nav className="ml-auto">
-          <Link to='/login'>
-            Admin Login
-          </Link>
+          <Link to="/login">Admin Login</Link>
         </Nav>
       </Navbar>
-
     </div>
-  )
+  );
 }
