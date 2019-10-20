f3d057ab74c9f7b6dd14b47ae363145db716067a
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-10-20 09:26:44 -0700

    using images on form buttons

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import TherapistForm from './Forms/TherapistForm.jsx'
 // import HomeScreen from './Views/HomeScreen.jsx'
 import Login from './Views/Login.jsx'
 
+import PatientImg from './assets/Illustrations/patient.svg'
+import TherapistImg from './assets/Illustrations/doctors.svg'
+
 import { Navbar, Nav, Button, Container, Row, Col } from 'react-bootstrap'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
@@ -33,13 +36,15 @@ function HomeScreen(){
       <Container className="text-center">
         <Row>
           <Col>
-            <Link to='/therapist'>
-              I am a therapist
+            <Link to='/patient'>
+              <img src={PatientImg} />
+              I am a patient
             </Link>
           </Col>
           <Col>
-            <Link to='/patient'>
-              I am a patient
+            <Link to='/therapist'>
+              <img src={TherapistImg} />
+              I am a therapist
             </Link>
           </Col>
         </Row>
