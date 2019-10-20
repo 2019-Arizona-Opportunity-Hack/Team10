cc07abed5270d644c69790325c1ea534340fb85d
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-10-19 18:44:58 -0700

    added ManagerDasboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import PatientForm from './Forms/PatientForm.jsx'
+import React from "react";
+import PatientForm from "./Forms/PatientForm.jsx";
+import ManagerDashboard from "./Views/ManagerDashboard";
 
 export default function App() {
   return (
     <div>
       <PatientForm />
+      <ManagerDashboard />
     </div>
   );
 }
