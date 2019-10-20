44a8425977e141c669653281d607d235b3dd2154
quincyshadow <inversenormal@gmail.com>
2019-10-20 12:51:13 -0700

    Update server.js
    app.use BEFORE routes HEADER of CORS ACCEPT *

--- server/server.js
+++ server/server.js
@@ -3,14 +3,16 @@ const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", `*`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+
 });
+
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
