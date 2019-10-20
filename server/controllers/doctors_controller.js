47dcaa583fec11891832baf379e6f9271899ee42
quincyshadow <inversenormal@gmail.com>
2019-10-20 11:47:05 -0700

    fixed server
    fixed server

--- server/controllers/doctors_controller.js
+++ server/controllers/doctors_controller.js
@@ -3,31 +3,37 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   fetchAll: (req, res) => {
-    knex('doctors')
-    .then((response) => {
-      res.json({ doctors: response })
-    })
+    knex("doctors").then(response => {
+      res.json({ doctors: response });
+    });
   },
   addOne: (req, res) => {
-    knex('doctors')
-    .insert({
-        fName: req.body.fName,
-        lName: req.body.lName,
-        phoneNum: req.body.phoneNum,
-        emailAddr: req.body.emailAddr
-    }, '*').then((res) => {
+    knex("doctors")
+      .insert(
+        {
+          fName: req.body.fName,
+          lName: req.body.lName,
+          phoneNum: req.body.phoneNum,
+          emailAddr: req.body.emailAddr
+        },
+        "*"
+      )
+      .then(res => {
         console.log(res[0].id);
-        return req.body.availableDoctorDates.map((availableDoctorDate) => {
-            knex('doctor_avails')
-            .insert({
+        return req.body.availableDoctorDates.map(availableDoctorDate => {
+          if (availableDoctorDate != null) {
+            knex("doctor_avails")
+              .insert({
                 startDate: availableDoctorDate.startDate,
                 endDate: availableDoctorDate.endDate,
                 doctor_id: res[0].id
-            }).then(() => {
+              })
+              .then(() => {
                 // res.sendStatus(200);
-                res.sendStatus('OK');
-            })
-        })
-    })
+                res.sendStatus("OK");
+              });
+          }
+        });
+      });
   }
-}
+};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function (err, req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", `*`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
