81b7a873fa89746e16657da33b8d0f25d84aa389
quincyshadow <inversenormal@gmail.com>
2019-10-20 08:54:21 -0700

    added patient, appts controller

--- server/controllers/appts_controller.js
+++ server/controllers/appts_controller.js
@@ -1,6 +1,28 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-  // CHANGE ME TO AN ACTUAL FUNCTION
-
-}
+	// CHANGE ME TO AN ACTUAL FUNCTION
+	fetchAll: (req, res) => {
+		knex("appointments").then(response => {
+			res.json({ appointments: response });
+		});
+	},
+	addOne: (req, res) => {
+		knex("appointments")
+			.insert(
+				{
+					startDate: req.body.startDate,
+					endDate: req.body.endDate,
+					patient_id: req.body.patient_id,
+					doctor_id: req.body.doctor_id
+				},
+				"*"
+			)
+			.then(res => {
+				console.log(res[0].id);
+				return req.body.startDate, req.body.endDate
+					}
+				);
+			});
+	}
+};
--- server/controllers/patients_controller.js
+++ server/controllers/patients_controller.js
@@ -2,7 +2,32 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
-  fetchAll: function(req, res) {
-  
+  fetchAll: (req, res) => {
+    knex('patients')
+    .then((response) => {
+      res.json({ patients: response })
+    })
   },
+  addOne: (req, res) => {
+    knex('patients')
+    .insert({
+        fName: req.body.fName,
+        lName: req.body.lName,
+        phoneNum: req.body.phoneNum,
+        emailAddr: req.body.emailAddr
+    }, '*').then((res) => {
+        console.log(res[0].id);
+        return req.body.availablePatientDates.map((availablePatientDate) => {
+            knex('doctor_avails')
+            .insert({
+                startDate: availablePatientDate.startDate,
+                endDate: availablePatientDate.endDate,
+                doctor_id: res[0].id
+            }).then(() => {
+                // res.sendStatus(200);
+                res.sendStatus('OK');
+            })
+        })
+    })
+  }
 }
